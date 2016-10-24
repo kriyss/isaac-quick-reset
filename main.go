76646885ab32b17373f07f3fd31e9f2a9576618d
kriyss <perso@kriyss.ninja>
2016-10-24 14:25:44 +0200

    refactor: remove windows selection

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"syscall"
 	"time"
-	"unsafe"
 
 	"github.com/AllenDang/w32"
 	"github.com/simulatedsimian/joystick"
@@ -18,13 +16,6 @@ const (
 	keyEventUp         = 0x0002
 )
 
-var (
-	user32                  = syscall.NewLazyDLL("user32.dll")
-	procSetForegroundWindow = user32.NewProc("SetForegroundWindow")
-	procEnumWindows         = user32.NewProc("EnumWindows")
-	procGetWindowTextW      = user32.NewProc("GetWindowTextW")
-)
-
 func exitOnError(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -33,11 +24,6 @@ func exitOnError(err error) {
 }
 
 func main() {
-	hwnd, err := findWindow("Binding of Isaac: Afterbirth")
-	exitOnError(err)
-	setForegroundWindow(hwnd)
-	time.Sleep(time.Second * 3)
-
 	js, err := joystick.Open(0)
 	exitOnError(err)
 
@@ -68,56 +54,4 @@ func pressKey(vk uint16, flags int) {
 			},
 		},
 	})
-}
-
-func setForegroundWindow(hwnd uintptr) bool {
-	ret, _, _ := procSetForegroundWindow.Call(hwnd)
-	return ret != 0
-}
-
-func findWindow(title string) (uintptr, error) {
-	var hwnd syscall.Handle
-	cb := syscall.NewCallback(func(h syscall.Handle, p uintptr) uintptr {
-		b := make([]uint16, 200)
-		_, err := getWindowText(h, &b[0], int32(len(b)))
-		if err != nil {
-			return 1 // continue enumeration
-		}
-		if syscall.UTF16ToString(b) == title {
-			// note the window
-			hwnd = h
-			return 0 // stop enumeration
-		}
-		return 1 // continue enumeration
-	})
-	enumWindows(cb, 0)
-	if hwnd == 0 {
-		return 0, fmt.Errorf("No window with title '%s' found", title)
-	}
-	return uintptr(hwnd), nil
-}
-
-func enumWindows(enumFunc uintptr, lparam uintptr) (err error) {
-	r1, _, e1 := syscall.Syscall(procEnumWindows.Addr(), 2, uintptr(enumFunc), uintptr(lparam), 0)
-	if r1 == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
-}
-
-func getWindowText(hwnd syscall.Handle, str *uint16, maxCount int32) (len int32, err error) {
-	r0, _, e1 := syscall.Syscall(procGetWindowTextW.Addr(), 3, uintptr(hwnd), uintptr(unsafe.Pointer(str)), uintptr(maxCount))
-	len = int32(r0)
-	if len == 0 {
-		if e1 != 0 {
-			err = error(e1)
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return
 }
